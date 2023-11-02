31dc04697a313911b4e1dfed6b7e0e29b580ed1a
Gavinorme <Gorme@email.sc.edu>
2023-11-02 12:43:10 -0400

    node

---
+++ public/script.js
@@ -0,0 +1,41 @@
+const showCharacters = async () =>
+{
+    let response = await fetch("http://localhost:3000/api/characters");
+    let characterJSON = await response.json();
+    let characterDiv = document.getElementById("characters-list");
+
+    characterJSON.forEach((character) => {
+        let section = document.createElement("section");
+        characterDiv.append(section);
+
+        let h3 = document.createElement("h3");
+        section.append(h3);
+        h3.innerHTML = character.name;
+
+        let p = document.createElement("p");
+        section.append(p);
+        p.innerHTML = ("Actor: " +character.actor);
+
+        let p1 = document.createElement("p");
+        section.append(p1);
+        p1.innerHTML = ("Role: " +character.role);
+
+        let p2 = document.createElement("p");
+        section.append(p2);
+        p2.innerHTML = ("Relation: "+character.relation);
+
+        let p3 = document.createElement("p");
+        section.append(p3);
+        p3.innerHTML = ("Attributes: "+character.attributes);
+
+        let img = document.createElement("img");
+        section.append(img);
+        img.src = "http://localhost:3000/"+ character.img;
+    });
+};
+
+
+window.onload = () =>
+{
+    showCharacters();
+};
--- server.js
+++ server.js
@@ -1,1 +1,61 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/characters", (req, res) => {
+    const characters = [];
+    characters[0] = {
+        name: "Rick",
+        actor: "Justin Roiland",
+        role: "Alcoholic scientist",
+        relation: "Grandpa",
+        attributes: ["reckless", " pessimistic", " megagenius", " courageous"],
+        img: "images/rick.png",
+    };
+
+    characters[1] = {
+        name: "Morty",
+        actor: "Justin Roiland",
+        role: "Sidekick",
+        relation: "Grandson",
+        attributes: ["anxious", " awkward", " doubtful", " caring"],
+        img: "images/morty.png",
+    };
+
+    characters[2] = {
+        name: "Jerry",
+        actor: "Chris Parnell",
+        role: "Dad",
+        relation: "Husband",
+        attributes: ["unintelligent", " pathetic", " pitiful", " insecure"],
+        img: "images/jerry.png",
+    };
+
+    characters[3] = {
+        name: "Beth",
+        actor: "Sarah Chalke",
+        role: "Mom",
+        relation: "Daughter",
+        attributes: ["ambitous", " independent", " intelligent", " insecure"],
+        img: "images/beth.png",
+    };
+
+    characters[4] = {
+        name: "Summer",
+        actor: "Spencer Grammer",
+        role: "Sister",
+        relation: "Granddaughter",
+        attributes: [" sarcastic", " energetic", " bold", " wity"],
+        img: "images/summer.png",
+    };
+
+    res.json(characters);
+});
+
+app.listen(3000, () => {
+    console.log("listening");
+});
