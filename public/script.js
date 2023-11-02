bf0a3e317115641bdafd6e512563cd85b9fbc79d
Gavinorme <Gorme@email.sc.edu>
2023-11-02 12:55:00 -0400

    node

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const showCharacters = async () =>
 {
-    let response = await fetch("http://localhost:3000/api/characters");
+    let response = await fetch("https://node-project-edxo.onrender.com");
     let characterJSON = await response.json();
     let characterDiv = document.getElementById("characters-list");
 
